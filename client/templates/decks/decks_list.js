9c28791133b5683be819f9fba635bda3623950bb
Chris Bradley <chrbradley (at) gmail.com>
2014-11-12 14:22:36 -0500

    added basic deck list template and static data/

---
+++ client/templates/decks/decks_list.js
@@ -0,0 +1,21 @@
+var decksData =[
+  {
+    title: 'Alogorithm of an Alogorithm',
+    date: 1
+  },
+  {
+    title: 'The Keyword This',
+    date: 9
+  },
+  {
+    title: 'How to succeed',
+    date: 15
+  },
+  {
+    title: 'Function Binding',
+    date: 2
+  }
+];
+Template.decksList.helpers({
+  decks: decksData
+});
