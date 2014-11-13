9d07e889c7529e0cc7d7cc48870eb08211d35ef5
Chris Bradley <chrbradley (at) gmail.com>
2014-11-13 14:36:18 -0500

    deck insert via server method, no dupicale deck titles

--- client/templates/decks/deck_submit.js
+++ client/templates/decks/deck_submit.js
@@ -5,7 +5,17 @@ Template.deckSubmit.events({
     var deck = {
       title: $(e.target).find('[name=title]').val()
     };
-    deck._id = Decks.insert(deck);
-    Router.go('deckPage', deck);
+
+    Meteor.call('deckInsert', deck, function(error, result) {
+      // display error to user and abort
+      if (error)
+        return alert(error.reason);
+      // show this result, but route anyway
+      if (result.deckExists)
+        alert('A deck with this title already exists.');
+      
+      Router.go('deckPage', {_id: result._id});
+    });
   }
+
 });
